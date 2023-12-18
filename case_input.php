2d9382d61c3536a74750235b50be412b7be812a3
JAYASURYA-DEVAN <144954653+JAYASURYA-DEVAN@users.noreply.github.com>
2023-12-18 06:38:31 +0530

    Add files via upload

---
+++ case_input.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Accust Details </title>
+</head>
+
+<body>
+
+<form action="algorithm1.php" method="post">
+
+<table width="300" border="0" cellspacing="1" cellpadding="1">
+  <tr>
+    <td>Enter the case id:</td>
+    <td> <input type="text" name="caseid"></td>
+  </tr>
+  <tr>
+    <td>Enter the case title: </td>
+    <td><input type="text" name="accussion"></td>
+  </tr>
+  <tr>
+    <td>Enter the accust name:</td>
+    <td> <input type="text" name="accust"></td>
+  </tr>
+<tr>
+    <td>Enter the case details:</td>
+    <td> <textarea name="case_det"> Enter the case details </textarea></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><input type="submit" ></td>
+  </tr>
+</table>
+
+</form>
+
+</body>
+
+</html>
+
